f15562a51bfca8e39c68a8904ebbd49adb96811c
Absaidov <dabsaidov@bk.ru>
2022-12-14 19:44:41 +0300

    Выполнение шага 3

--- app/src/main/java/hexlet/code/cli.java
+++ app/src/main/java/hexlet/code/cli.java
@@ -1,2 +1,15 @@
-package hexlet.code;public class cli {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class cli {
+    /*
+    * asking name of gamer
+    * */
+    public static String asking(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String s = sc.nextLine();
+        return "Hello, " + s + "!";
+    }
 }
