2385a5a88e97f876eb5997047d8bb2313c5a952b
JamaL <jamalikowow@mail.ru>
2023-01-19 21:42:10 +0300

    промежуточный коммит

--- app/src/main/java/hexlet/code/games/ArithProgression.java
+++ app/src/main/java/hexlet/code/games/ArithProgression.java
@@ -2,17 +2,23 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 
+import java.util.Arrays;
+
 import static hexlet.code.Engine.*;
 import static hexlet.code.GenerationOfRandomNumber.*;
 
 
 public class ArithProgression {
     public static String[] number;
+    public static String right2;
     public static int rightNumber;
     public static int randomNumber ;
+    public static String[] xxxForAnotherClass;
     public static String rightAnswer;
+
     public static String gameProgres(){
         questionOfGame = "What number is missing in the progression?";
+//        quizQuestion = rightAnswerMethod();
         return Engine.evenGaming();
     }
     public static String[] consecutiveNumbers() {
@@ -26,18 +32,35 @@ public class ArithProgression {
         int step = generationOfNumberForCalc4();
         while (i < randomNumberForAnotherClass + (step * lengthOfArray)) {
             i = i + step;
-            k = i + " ";
+            k = i + "";
             xxx[j] = k;
             j++;
         }
         rightNumber = generationOfNumberForProg();
-        rightAnswerOfTheGame = xxx[rightNumber] + "";
-        xxx[rightNumber] = ".. ";
+//        right2 = xxx[rightNumber];
+//        rightAnswerOfTheGame = xxx[rightNumber] + "";
+        rightAnswerOfTheGame = xxx[rightNumber];
+
+        xxx[rightNumber] = "..";
+        xxxForAnotherClass = xxx;
+//        quizQuestion = rightAnswerMethod();
         int l;
         for (l = 0; l < xxx.length; l++){
-//            System.out.print(xxx[l]);
-            quizQuestion = quizQuestion + xxx[l];
+            System.out.print(xxx[l] + " ");
+            var a = xxx[l];
+//            var b = xxx[l + 1];
+            quizQuestion = a;
+            quizQuestion = a + quizQuestion;
         }
         return xxx;
     }
+//    public static String rightAnswerMethod(){
+//        int l;
+//        for (l = 0; l < xxxForAnotherClass.length; l++){
+//            var a = xxxForAnotherClass[l] + " ";
+//            System.out.print(a);
+////            quizQuestion = quizQuestion + xxx[l] + " ";
+//        };
+//        return null;
+//    }
 }
