3128e5e47886902d4d8f37d6d1bd0a22b675bf59
Absaidov <dabsaidov@bk.ru>
2023-02-03 12:35:10 +0300

    fixing magicNumber in engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -7,7 +7,7 @@ import static hexlet.code.App.getNameForAnotherClass;
 
 public class Engine {
 
-    private static int numberOfRound = 3;
+    private static final int numberOfRound = 3;
     public static int getNumberOfRound() {
         return numberOfRound;
     }
