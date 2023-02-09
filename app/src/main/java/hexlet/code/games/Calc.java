b862f6a6a7926ba80fbe2cedd2686f37131af380
Absaidov <dabsaidov@bk.ru>
2023-02-09 12:25:24 +0300

    fixin Calcgame

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -10,41 +10,37 @@ import static hexlet.code.Engine.MAX_NUMBER_OF_NUMBERS;
 public class Calc {
     private static String description = "What is the result of the expression?";
     private static final int MINIMUM_NUMBER = 1;
-    private static final int MAXIMUM_NUMBER = 3;
 
     public static void resultOfGameCalc() {
         String[][] arrayData = new String[getNumberOfRound()][2];
+        int i = 0;
         int result;
         String question;
         for (int j = 0; j < arrayData.length; j++) {
             int randomNumber1 = generatesRandomNumbers(MINIMUM_NUMBER, MAX_NUMBER_OF_NUMBERS);
             int randomNumber2 = generatesRandomNumbers(MINIMUM_NUMBER, MAX_NUMBER_OF_NUMBERS);
-            int randomNumberCalc = generatesRandomNumbers(MINIMUM_NUMBER, MAXIMUM_NUMBER);
-            if (randomNumberCalc == 1) {
-                result = multi(randomNumber1, randomNumber2);
-                question = randomNumber1 + " * " + randomNumber2;
-            } else if (randomNumberCalc == 2) {
-                result = add(randomNumber1, randomNumber2);
-                question = randomNumber1 + " + " + randomNumber2;
-            } else {
-                result = sub(randomNumber1, randomNumber2);
-                question = randomNumber1 + " - " + randomNumber2;
-            }
+            String[] arrayOfOperators = new String[]{" - ", " * ", " + "};
+            question = resultForQuestion(randomNumber1,arrayOfOperators[i],randomNumber2);
+            result = resulOfExpression(randomNumber1,arrayOfOperators[i],randomNumber2);
             arrayData[j][0] = question;
             arrayData[j][1] = String.valueOf(result);
+            i++;
         }
         Engine.startEngine(description, arrayData);
     }
-    public static int randomOperator(int number1, int number2, int numberForOperator) {
+    public static String resultForQuestion(int number1, String numberForOperator, int number2) {
         return number1 + numberForOperator + number2;
     }
-    public static int add(int a, int b) {
-        return a + b;
-    }
-    public static int sub(int a, int b) {
-        return a - b;
-    }
-    public static int multi(int a, int b) {
-        return a * b;
+    public static int resulOfExpression(int number1, String numberForOperator, int number2) {
+        int result;
+        if (numberForOperator.equals(" - ")){
+            result = number1 - number2;
+        } else if (numberForOperator.equals(" * ")) {
+            result = number1 * number2;
+        }
+        else {
+            result = number1 + number2;
+        }
+        return result;
     }
 }
