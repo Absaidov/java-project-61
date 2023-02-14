7b8a99ff66a71c9e2c1d397728c37fa9c4f6c034
Absaidov <dabsaidov@bk.ru>
2023-02-14 15:24:38 +0300

    fix

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -8,7 +8,7 @@ public class Calc {
     private static final int MAX_NUMBER_OF_NUMBERS = 99;
     private static String description = "What is the result of the expression?";
     private static final int MINIMUM_NUMBER = 1;
-    private static String[] arrayOfOperators = new String[]{" - ", " * ", " + "};
+    private static String[] arrayOperators = new String[]{" - ", " * ", " + "};
 
     public static void calcGameStart() {
         String[][] arrayData = new String[NUMBER_OF_ROUND][2];
@@ -16,20 +16,20 @@ public class Calc {
         String question;
         for (int j = 0; j < arrayData.length; j++) {
             int result;
-            int randomOperator = generatesRandomNumbers(0, arrayOfOperators.length - 1);
+            int randomOperator = generatesRandomNumbers(0, arrayOperators.length - 1);
             int randomNumber1 = generatesRandomNumbers(MINIMUM_NUMBER, MAX_NUMBER_OF_NUMBERS);
             int randomNumber2 = generatesRandomNumbers(MINIMUM_NUMBER, MAX_NUMBER_OF_NUMBERS);
-            question = resultForQuestion(randomNumber1, arrayOfOperators[randomOperator], randomNumber2);
-            result = getExpression(randomNumber1, arrayOfOperators[randomOperator], randomNumber2);
+            question = getQuestion(randomNumber1, arrayOperators[randomOperator], randomNumber2);
+            result = calculateExpression(randomNumber1, arrayOperators[randomOperator], randomNumber2);
             arrayData[j][0] = question;
             arrayData[j][1] = String.valueOf(result);
         }
         Engine.startEngine(description, arrayData);
     }
-    private static String resultForQuestion(int number1, String numberForOperator, int number2) {
+    private static String getQuestion(int number1, String numberForOperator, int number2) {
         return number1 + numberForOperator + number2;
     }
-    private static int getExpression(int number1, String numberForOperator, int number2) {
+    private static int calculateExpression(int number1, String numberForOperator, int number2) {
         if (numberForOperator.equals(" - ")) {
             return number1 - number2;
         } else if (numberForOperator.equals(" * ")) {
